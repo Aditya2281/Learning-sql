044bc02c232eecaa0e04f5c7092a64ffbbe7df2e
aditya <adityadubey2281@gmail.com>
2025-06-10 22:30:10 -0700

    Update Join Queries.sql

--- Join Queries.sql
+++ Join Queries.sql
@@ -1,7 +1,17 @@
 select * from monarchs
 
 --Inner Join using On
-Select prime_ministers.country, prime_ministers.continent, prime_minister , president
-from presidents
-Inner join prime_ministers
-on presidents.country = prime_ministers.country
+SELECT
+	PRIME_MINISTERS.COUNTRY,
+	PRIME_MINISTERS.CONTINENT,
+	PRIME_MINISTER,
+	PRESIDENT
+FROM
+	PRESIDENTS
+	INNER JOIN PRIME_MINISTERS ON PRESIDENTS.COUNTRY = PRIME_MINISTERS.COUNTRY
+
+--Inner Join using Aliased Tables 
+Select p2.country , p2.continent, prime_minister, president 
+from presidents as p1
+INNER JOIN Prime_ministers as p2
+on p1.country = p2.country
