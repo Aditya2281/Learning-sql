83f0a981fafba5e6ddd17497904992b5d6fdfbcf
aditya <adityadubey2281@gmail.com>
2025-06-10 22:21:41 -0700

    Update Join Queries.sql

--- Join Queries.sql
+++ Join Queries.sql
@@ -1,1 +1,7 @@
 select * from monarchs
+
+--Inner Join using On
+Select prime_ministers.country, prime_ministers.continent, prime_minister , president
+from presidents
+Inner join prime_ministers
+on presidents.country = prime_ministers.country
