6ce1a98d90ec3335b662a28e3071816bb2d270c6
aditya <adityadubey2281@gmail.com>
2025-06-30 01:10:30 -0700

    chff

--- Queries.sql
+++ Queries.sql
@@ -1,6 +1,1 @@
-SELECT id, title, release_year, country, duration, language, certification, gross, budget
-	FROM public.films;
-
-	select aditya;
-   
-   SELECT  title from films;SELECT * FROM STOCK
+SELECT * FROM STOCK
