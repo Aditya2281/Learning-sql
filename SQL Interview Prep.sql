fba47845f89299af07350096718f96aac523e782
aditya <adityadubey2281@gmail.com>
2025-07-02 00:46:51 -0700

    Update SQL Interview Prep.sql

--- SQL Interview Prep.sql
+++ SQL Interview Prep.sql
@@ -10,3 +10,20 @@ GROUP BY
 	USER_ID
 HAVING
 	COUNT(DISTINCT TYPE) =2
+
+
+---We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. Find the current salary of each employee assuming that salaries increase each year. 
+--Output their id, first name, last name, department ID, and current salary. Order your list by employee ID in ascending order.
+SELECT 
+    id,
+    first_name,
+    last_name,
+    department_id,
+    salary
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (PARTITION BY id ORDER BY salary DESC) AS rn
+    FROM ms_employee_salary
+) sub
+WHERE rn = 1
+ORDER BY id;
