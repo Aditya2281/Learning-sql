88146e16001a3f0fb564365c8acc25ab5ca2929c
aditya <adityadubey2281@gmail.com>
2025-07-03 22:36:26 -0700

    Update Join Queries.sql

--- Join Queries.sql
+++ Join Queries.sql
@@ -148,6 +148,26 @@ SELECT
   ROUND(revenue :: NUMERIC / GREATEST(users, 1), 2) AS arpu 
 FROM kpis;
 
+--Query I (by month):
+WITH kpis AS (
+  SELECT 
+    DATE_TRUNC('month', order_date) AS delivr_month,
+    SUM(meal_price * order_quantity) AS revenue,
+    COUNT(DISTINCT user_id) AS users
+  FROM meals m
+  JOIN orders o ON m.meal_id = o.meal_id
+  GROUP BY delivr_month
+)
+SELECT 
+  delivr_month,
+  ROUND(
+    revenue :: NUMERIC / GREATEST(users, 1),
+  2) AS arpu
+FROM kpis
+ORDER BY delivr_month ASC;
+
+
+
 
 
 
